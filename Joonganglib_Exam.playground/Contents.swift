19aa874ab57868fb5079370241a46e097aa93c53
Sulkun <flypig1234@gmail.com>
2021-03-08 23:18:47 +0900

    7번쨰 푸시할때 마다 자꾸 브랜치가 생김

--- Joonganglib_Exam.playground/Contents.swift
+++ Joonganglib_Exam.playground/Contents.swift
@@ -13,5 +13,7 @@ var str4 = "네번째 이름 변경"
 var str5 = "네번째 zeroin"
 
 var str6 = "다섯번쨰 클론으로 다운받았다"
-
+ㄴ
 var str7 = "여섯번쨰 키체인에서 Source 검색후 계정 삭제 And 소스트리 메뉴에서 설정 >> git 시스템깃 사용 git 더블클릭"
+
+var str8 = "7번쨰 푸시할때 마다 자꾸 브랜치가 생김"
